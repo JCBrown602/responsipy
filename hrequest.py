065b2b8797a7c97344f1d96d866de8d7bbb5fef6
Jason <jason@AlmasFridge>
2020-12-13 22:39:17 -0800

    Lots of work. Response is stuck at global def.

--- hrequest.py
+++ hrequest.py
@@ -1,5 +1,15 @@
-# /responsipy Response Tool
-# Content-Type, Port Number
+"""
+Response Tool
+https://github.com/JCBrown602/responsipy
+Command line (*nix) tool that asks for URL and gets HEADERS
+for that URL.
+
+TODO:
+ - Isolate Content-Type
+ - Open text file containing URLs and get Content-Type for all of them
+    - Could be 100+ URLs
+ - Get port number? Available in requests module?
+"""
 
 # Import requests module
 import requests
@@ -9,23 +19,58 @@ import requests
 # url file, response
 url = 'https://example.com'
 response = requests.get(url)
+strError = 'UNDEFINED'
 
 # User input for url file name OR url
-print("Enter something: ", end = '')
-input1 = input()
+def getInput():
+    print("Enter something: ", end = '')
+    input1 = input()
+    checkInput(input1)
+    print("### getInput().input1 = ", input1)
+    return input1
+
+def getRespo():
+    pass
+
+def showMsg(strError):
+    print("*** ", strError, ": Must enter a URL starting with http:// or https:// ***")
+
+# Error handling
+def checkInput(input1):
+    # 'is not None' doesn't catch an empty/blank string
+    print("### checkInput().input1 BEFORE the try = ", input1)
+    if input1:
+        # if user entered url, treat it as a single "url"
+        try:
+            #response = requests.get(input1)
+            response = requests.head(input1)
+        except requests.exceptions.MissingSchema:
+            strError = 'MissingSchema'
+            showMsg(strError)
+            getInput()
+        except:
+            showMsg()
+            getInput()
+    else:
+        print("*** Something happened, but it was wonky. Try again. ***")
+        getInput()
+    #print("CHECKED input was: ", input1)
+    print("### checkInput().input1 = ", input1)
+    return response
 
 # Loop through url.txt/url and print Content-Type
 def responseLoop():
-    print("RESPONSE: ", response)
-    print("HEADERS: ", response.headers)
+    getInput()
+    print("-")
+    print("URL: ", response.url)
+    print("SERVER: ", response.headers['server'])
+    print("RESPONSE: ", response.status_code)
+    print("CONTENT-TYPE: ", response.headers['content-type'])
+    print("-")
+
+# Start the loop
+responseLoop()
 
 # IF logic
-if input1 is not None:
-    # if user entered url, treat it as a single "url"
-    response = requests.get(input1)
-    print("Single URL: ", input1)
-    responseLoop()
-else:
     # if user entered file, treat it as a "url.txt"
     # Open and read url.txt to var
-    responseLoop()
